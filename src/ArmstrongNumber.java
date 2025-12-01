ab2f0a2cb056bd69dd7b66c11af00fdc3725dec1
fitnessscoder <fitnessscoder@gmail.com>
2025-12-01 21:03:57 +0530

    1-Dec-25 : Check Armstrong Number based on Extraction of Digits

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        System.out.println("Main method");
+
+        Scanner sc = new Scanner(System.in); // Create Scanner object
+
+        System.out.print("Enter number of your choice: ");
+        int number = sc.nextInt(); // Read integer input
+
+        System.out.println("Digits to be armstronged from : " +number);
+        checkArmStrongNumber(number);
+        sc.close(); // Close
+    }
+
+    private static void checkArmStrongNumber(int number) {
+        int sum=0;
+        int duplicate=number;
+        while(number>0) {
+            int lastDigit=number%10;
+            sum=sum+(lastDigit*lastDigit*lastDigit);
+            number=number/10;
+        }
+        if(duplicate==sum) {
+            System.out.println("The number "+duplicate +" is an Armstrong Number");
+        } else {
+            System.out.println("The number "+duplicate +" is NOT an Armstrong Number");
+        }
+    }
+}
