0799e5848e9905b2fd82001059fb06a2a252a243
fitnessscoder <fitnessscoder@gmail.com>
2025-12-30 13:54:25 +0530

    30-Dec-25 : print name N times

---
+++ src/recursion/PrintName.java
@@ -0,0 +1,23 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class PrintName {
+    public static void main(String[] args) {
+        System.out.println("Main method");
+
+        Scanner sc = new Scanner(System.in); // Create Scanner object
+
+        System.out.print("Enter number of your choice: ");
+        int number = sc.nextInt(); // Read integer input
+
+        printNameNTimes(1,number);
+        sc.close(); // Close
+    }
+
+    private static void printNameNTimes(int i, int number) {
+        if(i>number) return;
+        System.out.println("Vishal");
+        printNameNTimes(i+1,number);
+    }
+}
