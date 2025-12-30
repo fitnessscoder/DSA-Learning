5880f545497eeebd695cdc3e4a39f60ef205362f
fitnessscoder <fitnessscoder@gmail.com>
2025-12-30 13:56:27 +0530

    30-Dec-25 : print from 1 to N linearly

---
+++ src/recursion/PrintLinear.java
@@ -0,0 +1,24 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class PrintLinear {
+    public static void main(String[] args) {
+        System.out.println("Main method");
+
+        Scanner sc = new Scanner(System.in); // Create Scanner object
+
+        System.out.print("Enter number of your choice: ");
+        int number = sc.nextInt(); // Read integer input
+
+        printTillN(1,number);
+        sc.close(); // Close
+
+    }
+
+    private static void printTillN(int i, int number) {
+        if(i>number) return;
+        System.out.println(i);
+        printTillN(i+1, number);
+    }
+}
